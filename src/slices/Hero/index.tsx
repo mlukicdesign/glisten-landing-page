c72b908f49a2a2998fce49008c8d77ec9a6f2506
mlukicdesign <mlukicdesign@gmail.com>
2024-05-14 20:09:06 +0800

    homepage component

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,30 @@
+import ButtonLink from "@/components/ButtonLink";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      <ButtonLink field={slice.primary.button_link}>
+        Link
+      </ButtonLink>
+      <PrismicNextImage field={slice.primary.image} />
+    </section>
+  );
+};
+
+export default Hero;
